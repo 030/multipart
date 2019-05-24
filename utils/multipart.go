aacb87ab36a777a89a855021e3d55c79f394c60a
030 <chocolatey030@gmail.com>
2019-05-24 21:19:29 +0200

    [GH-9] test readFile function

--- utils/multipart.go
+++ utils/multipart.go
@@ -1,5 +1,13 @@
 package utils
 
-func readFile() string {
-	return "helloworld"
+import (
+	"io/ioutil"
+)
+
+func readFile(f string) (string, error) {
+	dat, err := ioutil.ReadFile(f)
+	if err != nil {
+		return "", err
+	}
+	return string(dat), nil
 }
