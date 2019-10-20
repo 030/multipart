19fa8a2f17242d30b5367284085eaf49f124dada
030 <chocolatey030@gmail.com>
2019-10-20 16:47:34 +0200

    [GH-16] Improved multipartBody test

--- utils/multipart.go
+++ utils/multipart.go
@@ -3,12 +3,8 @@ package utils
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"mime/multipart"
-	"net/http"
 	"strings"
-
-	log "github.com/sirupsen/logrus"
 )
 
 var body = new(bytes.Buffer)
@@ -21,117 +17,142 @@ type Upload struct {
 	Password string
 }
 
-// MultipartUpload splits the string into a slice, created a multipart
-// and that is posted to an URL
-func (u Upload) MultipartUpload(s string) error {
+// // MultipartUpload splits the string into a slice, creates a multipart
+// // and that is posted to an URL
+// func (u Upload) MultipartUpload(s string) error {
+// 	args := strings.Split(s, ",")
+// 	err := multipartBody(args...)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = u.upload()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
+func stringToSlice(s string) []string {
 	args := strings.Split(s, ",")
-	fmt.Println(args)
-	err := multipartBody(args...)
-	if err != nil {
-		return err
-	}
-	fmt.Println("CP2")
-	err2 := u.upload()
-	if err2 != nil {
-		return err2
-	}
-	return nil
+	return args
 }
 
-func addFileToWriter(b []byte, fn, f string) error {
-	part, err := writer.CreateFormFile(fn, f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err2 := part.Write(b)
-	if err2 != nil {
-		return err2
-	}
-	return nil
-}
-
-func addKeyValueToWriter(k, v string) error {
-	err := writer.WriteField(k, v)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func readFile(f string) ([]byte, error) {
-	b, err := ioutil.ReadFile(f)
-	if err != nil {
-		return nil, err
-	}
-	return b, nil
-}
-
-func writeField(s string) string {
-	parts := strings.Split(s, "=")
-	return parts[0] + " " + parts[1]
-}
-
-func multipartBody(f ...string) error {
-	fmt.Println("CP2")
-	log.Debug("The input string: ", f)
-	for _, v := range f {
-		log.Debug("The elements that reside in the input string: ", v)
-
+func multipartBody(s []string) (string, string, error) {
+	var metadata, file string
+	var parts []string
+	for _, v := range s {
 		if strings.Contains(v, "=@") {
-			fmt.Println("CP3")
-			parts := strings.Split(v, "=@")
-			fmt.Println("CP3a")
-			b, err := ioutil.ReadFile(parts[1])
-			fmt.Println("CP3b")
-			if err != nil {
-				return err
-			}
-			fmt.Println("CP3c")
-			addFileToWriter(b, parts[0], parts[1])
-			fmt.Println("CP3d")
+			parts = strings.Split(v, "=@")
+			metadata = parts[0]
+			file = parts[1]
+		} else if strings.Contains(v, "=") {
+			parts = strings.Split(v, "=")
+			metadata = parts[0]
+			file = parts[1]
 		} else {
-			fmt.Println("CP4")
-			fmt.Println(v)
-			parts := strings.Split(v, "=")
-			fmt.Println(len(parts))
-			if len(parts) == 1 {
-				return fmt.Errorf("The string should at least contain a '=', but was: '%v'", parts)
-			}
-			fmt.Println("CP4b")
-
-			err := addKeyValueToWriter(parts[0], parts[1])
-			if err != nil {
-				return err
-			}
+			return "", "", fmt.Errorf("The string should at least contain a '=', but was: '%v'", parts)
 		}
 	}
-
-	err := writer.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return metadata, file, nil
 }
 
-func (u Upload) upload() error {
-	req, err := http.NewRequest("POST", u.URL, body)
-	if err != nil {
-		return err
-	}
-	req.Header.Add("Content-Type", writer.FormDataContentType())
-	req.SetBasicAuth(u.Username, u.Password)
+// func addFileToWriter(b []byte, fn, f string) error {
+// 	part, err := writer.CreateFormFile(fn, f)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+// 	_, err = part.Write(b)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-	b, err := ioutil.ReadAll(resp.Body)
-	if !(resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusNoContent) || (err != nil) {
-		return fmt.Errorf("HTTPStatusCode: '%d'; ResponseMessage: '%s'; ErrorMessage: '%v'", resp.StatusCode, string(b), err)
-	}
-	return nil
-}
+// func addKeyValueToWriter(k, v string) error {
+// 	err := writer.WriteField(k, v)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
+
+// func readFile(f string) ([]byte, error) {
+// 	b, err := ioutil.ReadFile(f)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return b, nil
+// }
+
+// // func writeField(s string) (string, error) {
+// // 	parts := strings.Split(s, "=")
+// // 	if len(parts) == 1 {
+// // 		return "", fmt.Errorf("The string should at least contain a '=', but was: '%v'", parts)
+// // 	}
+// // 	return parts[0] + " " + parts[1], nil
+// // }
+
+// func multipartBody(f ...string) error {
+// 	fmt.Println("CP2")
+// 	log.Debug("The input string: ", f)
+// 	for _, v := range f {
+// 		log.Debug("The elements that reside in the input string: ", v)
+
+// 		if strings.Contains(v, "=@") {
+// 			fmt.Println("CP3")
+// 			parts := strings.Split(v, "=@")
+// 			fmt.Println("CP3a")
+// 			b, err := ioutil.ReadFile(parts[1])
+// 			fmt.Println("CP3b")
+// 			if err != nil {
+// 				return err
+// 			}
+// 			fmt.Println("CP3c")
+// 			addFileToWriter(b, parts[0], parts[1])
+// 			fmt.Println("CP3d")
+// 		} else {
+// 			fmt.Println("CP4")
+// 			fmt.Println(v)
+// 			parts := strings.Split(v, "=")
+// 			fmt.Println(len(parts))
+// 			if len(parts) == 1 {
+// 				return fmt.Errorf("The string should at least contain a '=', but was: '%v'", parts)
+// 			}
+// 			fmt.Println("CP4b")
+
+// 			err := addKeyValueToWriter(parts[0], parts[1])
+// 			if err != nil {
+// 				return err
+// 			}
+// 		}
+// 	}
+
+// 	err := writer.Close()
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+// func (u Upload) upload() error {
+// 	req, err := http.NewRequest("POST", u.URL, body)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	req.Header.Add("Content-Type", writer.FormDataContentType())
+// 	req.SetBasicAuth(u.Username, u.Password)
+
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	b, err := ioutil.ReadAll(resp.Body)
+// 	if !(resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusNoContent) || (err != nil) {
+// 		return fmt.Errorf("HTTPStatusCode: '%d'; ResponseMessage: '%s'; ErrorMessage: '%v'", resp.StatusCode, string(b), err)
+// 	}
+// 	return nil
+// }
--- utils/multipart_test.go
+++ utils/multipart_test.go
@@ -1,79 +1,138 @@
 package utils
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestReadFile(t *testing.T) {
-	got, err := readFile("test-files/helloworld.txt")
-	if err != nil {
-		t.Errorf("Cannot read file: %v; ", err)
-	}
-	want := "helloworld"
-	if string(got) != want {
-		t.Errorf("readFile() = %s; want %s", got, want)
+func TestStringToSlice(t *testing.T) {
+	testMap := map[string][]string{
+		"":                []string{""},
+		"hello,world":     []string{"hello", "world"},
+		"hello,world,ola": []string{"hello", "world", "ola"},
 	}
 
-	_, err2 := readFile("folder-does-not-exist/helloworld.txt")
-	want2 := "open folder-does-not-exist/helloworld.txt: no such file or directory"
-	if err2.Error() != want2 {
-		t.Errorf("An error was expected. Received '%v'; want '%s'", err2, want2)
+	for k, v := range testMap {
+		got := stringToSlice(k)
+		want := v
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("Slice not identical. Expected %s, but was %s.", want, got)
+		}
 	}
 }
 
-func TestMultipart(t *testing.T) {
-	err := multipartBody("maven2.asset1=@test-files-multipart/file1.pom",
-		"maven2.asset1.extension=pom",
-		"maven2.asset2=@test-files-multipart/file1.jar",
-		"maven2.asset2.extension=jar",
-		"maven2.asset3=@test-files-multipart/file1-sources.jar",
-		"maven2.asset3.extension=sources.jar")
-	if err != nil {
-		t.Errorf("Unexpected error; got '%v'", err)
+func TestMultipartBody(t *testing.T) {
+	// happy 1 - metadata and file
+	in := []string{"maven2.asset1=@test-files-multipart/file1.pom"}
+
+	got1, got2, _ := multipartBody(in)
+	want1 := "maven2.asset1"
+	want2 := "test-files-multipart/file1.pom"
+
+	if got1 != want1 || got2 != want2 {
+		t.Errorf("Got: '%s' && '%s', want '%s' && '%s'", got1, got2, want1, want2)
 	}
 
-	err = multipartBody("something.some=@test-files-multipart/does-not-exist.txt")
-	if err == nil {
-		t.Errorf("Expected error, got '%v'", err)
+	// happy 2 - metadata and extension
+	in = []string{"maven2.asset1.extension=pom"}
+
+	got1, got2, _ = multipartBody(in)
+	want1 = "maven2.asset1.extension"
+	want2 = "pom"
+
+	if got1 != want1 || got2 != want2 {
+		t.Errorf("Got: '%s' && '%s', want '%s' && '%s'", got1, got2, want1, want2)
 	}
 
-	err = multipartBody("hello,world")
-	want := "The string should at least contain a '=', but was: '[hello,world]'"
-	if err.Error() != want {
-		t.Errorf("Expected error, got '%v', want: '%v'", err, want)
-	}
-}
+	// unhappy
+	in = []string{"this is invalid input"}
 
-func TestWriteField(t *testing.T) {
-	got := writeField("maven2.asset3.extension=jar")
-	want := "maven2.asset3.extension jar"
-	if got != want {
-		t.Errorf("Want '%v', got '%v'", want, got)
-	}
-}
+	_, _, err := multipartBody(in)
+	want := "The string should at least contain a '=', but was: '[]'"
 
-func TestUpload(t *testing.T) {
-	u := Upload{URL: "", Username: "", Password: ""}
-	err := u.upload()
-	want := "Post : unsupported protocol scheme \"\""
 	if err.Error() != want {
 		t.Errorf("An error was expected. Received '%v'; want '%s'", err, want)
 	}
-
-	u = Upload{URL: "http://releasesoftwaremoreoften.com", Username: "admin", Password: "incorrect password"}
-	err = u.upload()
-	want = `HTTPStatusCode: '405'; ResponseMessage: '<html>
-<head><title>405 Not Allowed</title></head>
-<body bgcolor="white">
-<center><h1>405 Not Allowed</h1></center>
-</body>
-</html>'; ErrorMessage: '<nil>'`
-	if err.Error() != want {
-		t.Errorf("An error was expected. Got: '%v'; want: '%s'", err, want)
-	}
 }
 
-// func TestMultipartUpload(t *testing.T) {
-// 	u := Upload{URL: "", Username: "", Password: ""}
-// 	u.MultipartUpload("hello,world")
+// func TestReadFile(t *testing.T) {
+// 	got, err := readFile("test-files/helloworld.txt")
+// 	if err != nil {
+// 		t.Errorf("Cannot read file: %v; ", err)
+// 	}
+// 	want := "helloworld"
+// 	if string(got) != want {
+// 		t.Errorf("readFile() = %s; want %s", got, want)
+// 	}
+
+// 	_, err = readFile("folder-does-not-exist/helloworld.txt")
+// 	want = "open folder-does-not-exist/helloworld.txt: no such file or directory"
+// 	if err.Error() != want {
+// 		t.Errorf("An error was expected. Received '%v'; want '%s'", err, want)
+// 	}
 // }
+
+// func TestMultipartBody(t *testing.T) {
+// 	err := multipartBody("maven2.asset1=@test-files-multipart/file1.pom",
+// 		"maven2.asset1.extension=pom",
+// 		"maven2.asset2=@test-files-multipart/file1.jar",
+// 		"maven2.asset2.extension=jar",
+// 		"maven2.asset3=@test-files-multipart/file1-sources.jar",
+// 		"maven2.asset3.extension=sources.jar")
+// 	if err != nil {
+// 		t.Errorf("Unexpected error; got '%v'", err)
+// 	}
+
+// 	err = multipartBody("something.some=@test-files-multipart/does-not-exist.txt")
+// 	if err == nil {
+// 		t.Errorf("Expected error, got '%v'", err)
+// 	}
+
+// 	err = multipartBody("hello,world")
+// 	want := "The string should at least contain a '=', but was: '[hello,world]'"
+// 	if err.Error() != want {
+// 		t.Errorf("Expected error, got '%v', want: '%v'", err, want)
+// 	}
+// }
+
+// // func TestWriteField(t *testing.T) {
+// // 	// happy
+// // 	got, _ := writeField("maven2.asset3.extension=jar")
+// // 	want := "maven2.asset3.extension jar"
+// // 	if got != want {
+// // 		t.Errorf("Want '%v', got '%v'", want, got)
+// // 	}
+
+// // 	// unhappy
+// // 	_, err := writeField("haha")
+// // 	want = "The string should at least contain a '=', but was: '[haha]'"
+// // 	if err.Error() != want {
+// // 		t.Errorf("Expected error, got '%v', want: '%v'", err, want)
+// // 	}
+// // }
+
+// func TestUpload(t *testing.T) {
+// 	u := Upload{URL: "", Username: "", Password: ""}
+// 	err := u.upload()
+// 	want := "Post : unsupported protocol scheme \"\""
+// 	if err.Error() != want {
+// 		t.Errorf("An error was expected. Received '%v'; want '%s'", err, want)
+// 	}
+
+// 	u = Upload{URL: "http://releasesoftwaremoreoften.com", Username: "admin", Password: "incorrect password"}
+// 	err = u.upload()
+// 	want = `HTTPStatusCode: '405'; ResponseMessage: '<html>
+// <head><title>405 Not Allowed</title></head>
+// <body bgcolor="white">
+// <center><h1>405 Not Allowed</h1></center>
+// </body>
+// </html>'; ErrorMessage: '<nil>'`
+// 	if err.Error() != want {
+// 		t.Errorf("An error was expected. Got: '%v'; want: '%s'", err, want)
+// 	}
+// }
+
+// // func TestMultipartUpload(t *testing.T) {
+// // 	u := Upload{URL: "", Username: "", Password: ""}
+// // 	u.MultipartUpload("hello,world")
+// // }
